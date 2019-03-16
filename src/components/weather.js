85abc90e6718d9c2eec8a139e51135fae9f0c461
Zaur Tigiev <zaurtigiev@yandex.ru>
2019-03-16 17:54:00 +0300

    show weather param

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,15 @@ class Weather extends React.Component {
   render(){
     return (
       <div>
-          
+        {this.props.city &&
+          <div>
+            <p>Местоположение: {this.props.city}, {this.props.country}</p>
+            <p>Температура: {this.props.temp}</p>
+            <p>Давление: {this.props.pressure}</p>
+            <p>Заход солнца: {this.props.sunset}</p>
+          </div>
+        }
+        <p>{this.props.error}</p>
       </div>
     );
   }
